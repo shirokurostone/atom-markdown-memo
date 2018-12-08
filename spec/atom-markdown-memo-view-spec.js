d544c7fb2025941d1e25dd32706fece5304a225c
shirokurostone <shirokurostone@gmail.com>
2018-12-08 22:26:31 +0900

    update package

--- spec/atom-markdown-memo-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomMarkdownMemoView from '../lib/atom-markdown-memo-view';
-
-describe('AtomMarkdownMemoView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
